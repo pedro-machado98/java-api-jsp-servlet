ea30eb17f6206e63f2f13f68f5f04c080e42a09c
Pedro <pedromachado2298@gmail.com>
2023-11-01 23:16:38 -0300

    atualizando

--- EstoqueDeProdutos/src/main/java/com/pedromachado/estoque/controller/ProductController.java
+++ EstoqueDeProdutos/src/main/java/com/pedromachado/estoque/controller/ProductController.java
@@ -11,6 +11,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
@@ -37,6 +38,28 @@ public class ProductController extends HttpServlet {
         PrintWriter pw = resp.getWriter();
         pw.print(gson.toJson(productDAO.getAll()));
 
+        pw.flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("utf-8");
+        req.setCharacterEncoding("utf-8");
+        resp.setStatus(200);
+
+        StringBuffer sb = new StringBuffer();
+
+        BufferedReader br = req.getReader();
+        String attributes = null;
+
+        while ((attributes = br.readLine()) != null) {
+            sb.append(attributes);
+        }
+        Product prod = gson.fromJson(sb.toString(), Product.class);
+
+        PrintWriter pw = resp.getWriter();
+        pw.print(gson.toJson(productDAO.saveProduct(prod)));  //response is a object of type product
         pw.flush();
     }
 }
--- EstoqueDeProdutos/src/main/java/com/pedromachado/estoque/service/ProductService.java
+++ EstoqueDeProdutos/src/main/java/com/pedromachado/estoque/service/ProductService.java
@@ -5,10 +5,7 @@ import com.pedromachado.estoque.db.DbConfig;
 import com.pedromachado.estoque.db.DbException;
 import com.pedromachado.estoque.entity.Product;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -51,6 +48,38 @@ public class ProductService implements ProductDAO {
 
     @Override
     public Product saveProduct(Product product) {
+        String saveProduct = "insert into estoque (name, price, quantity) values (?, ?, ?)";
+        this.conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try{
+            conn=DbConfig.connect();
+            ps = conn.prepareStatement(saveProduct, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, product.getName());
+            ps.setDouble(2, product.getPrice());
+            ps.setInt(3, product.getQuantity());
+
+
+            int affectedRows = ps.executeUpdate();
+
+            if (affectedRows > 0) {
+                rs = ps.getGeneratedKeys();
+                if(rs.next()) {
+                    System.out.println(rs.getMetaData());
+                    System.out.println(rs.getDouble(3));
+                    System.out.println(rs.getString(2));
+                    Product prod = new Product(rs.getInt(1), rs.getString(2), rs.getDouble(3), rs.getInt(4));
+                    return prod;
+                }
+            }
+
+        } catch (Exception e) {
+            throw new DbException(e.getMessage(), e.getCause());
+        } finally {
+            DbConfig.closePreparedStatement(ps);
+            DbConfig.closeResultSet(rs);
+        }
         return null;
     }
 
